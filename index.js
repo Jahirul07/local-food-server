342d16dcb74fda5aea4fbd2b3cea94851071d676
Jahirul Islam <jahirul@elyranur.com>
2022-12-20 01:47:24 +0600

    all service sent clint

--- index.js
+++ index.js
@@ -13,12 +13,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rebcmop.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const FoodCollection = client.db('localFoods').collection('foods');
+
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = FoodCollection.find(query);
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/foods/:id', async(req, res) =>{
+            
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) =>{
     res.send('local food server in running');
